c29ccd46ba6c63af677a4de777728910e59a2f63
Rohit Bajpai <83673709+Bajpai1201@users.noreply.github.com>
2024-11-29 15:55:08 +0000

    datatypes examples

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3); we are using node.js not browser
+
+let name = "rohit"; //string
+
+let age = 26; //number
+
+let isLoggedin = false; //boolean
+/*
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+object
+JavaScript primarily has six basic data types: Number, String, Boolean, Undefined, Null, and Object. Additionally, ES6 introduced Symbol for unique identifiers, and ES2020 introduced BigInt for large integers. 
+
+ECMAScript standards are crucial as they define the specifications for JavaScript. Key milestones include ES6 in 2015, which introduced major features like arrow functions, let/const, and classes. ES2017 brought in async/await for better asynchronous programming.
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
