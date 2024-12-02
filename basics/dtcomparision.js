2b4501b6a596802b944a64ca35185e2dc2786eab
Rohit Bajpai <83673709+Bajpai1201@users.noreply.github.com>
2024-12-02 11:31:45 +0000

    datatypes comparision in javascript

---
+++ basics/dtcomparision.js
@@ -0,0 +1,22 @@
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+/*console.log(null >= 0); true
+console.log(null == 0); false
+console.log(null < 0); false
+
+console.log(undefined > 0); false
+console.log(undefined == 0); false
+console.log(undefined < 0); false
+*/
+
+// === ye operator direct conversion na kr k pahle datatype check krta h
+
+//console.log("2" === 2);
+
+
+
+
+
+
+
