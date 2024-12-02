5512091dca9a82f76112398834f6080991f453e1
Rohit Bajpai <83673709+Bajpai1201@users.noreply.github.com>
2024-12-02 10:12:14 +0000

    string to number conversion explained with some basic operations

--- basics/dtconversion.js
+++ basics/dtconversion.js
@@ -1,10 +1,10 @@
 let score = true; //string jo bhi double quotes me hota h wo string h
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueinNumber = Number(score);
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
+//console.log(typeof valueinNumber);
+//console.log(valueinNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,4 +14,39 @@ console.log(valueinNumber);
 // "rohit" => true
 let isLoggedin = "rohit";
 let booleanisLoggedin = Boolean(isLoggedin);
-console.log(booleanisLoggedin);
+//console.log(booleanisLoggedin);
+
+// ****************OPERATIONS*************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+/*
+console.log(2+2);
+console.log(3-2);
+console.log(2*2); // multiplication
+console.log(2**2); //power
+console.log(2/3); //division
+console.log(2%3); // remainder
+*/
+
+let str1 = "hello";
+let str2 = " rohit";
+let str3 = str1 + str2;
+
+//console.log(str3);
+//console.log("1" + 2); // output => 12
+//console.log(1 + 3 + "2"); // number pahle or string "" baad me hoga to pahle value me addition perform hoga fir string add hogi output => 42
+//console.log(true); => true
+//console.log(+true); 1
+//console.log(+""); 0
+
+let gameCounter = 100;
+gameCounter++; 
+console.log(gameCounter);
+
+
+
+
+
+
