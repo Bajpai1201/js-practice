dea57299094662d1573cc0f5c7e7c88d8e871561
Rohit Bajpai <83673709+Bajpai1201@users.noreply.github.com>
2024-12-10 11:03:07 +0000

    object advance methods

--- basics/objects.js
+++ basics/objects.js
@@ -1,6 +1,6 @@
-// singleton
 
-//object literals
+
+//----------object literals start----------
 //Object.create another way to create a object
 
 const mySym = Symbol("key1") 
@@ -25,17 +25,75 @@ jsUser.email = "rohit@gmail.com";
 jsUser.email = "rohit@chatgpt.com";
 // console.log(jsUser);
 jsUser.greetings = function(){
-    console.log("Hello JS User");
+    //console.log("Hello JS User");
 }
 
-console.log(jsUser.greetings());
+//console.log(jsUser.greetings());
 
 jsUser.greeting = function(){
-    console.log(`Hello JS User, ${this.name}`);
+ //   console.log(`Hello JS User, ${this.name}`);
 }
 
-console.log(jsUser.greeting());
-console.log(jsUser.greetings());
+//console.log(jsUser.greeting());
+//console.log(jsUser.greetings());
+
+//------object literal end----
+
+//-------singleton start--------
+
+//const tinderUser = new Object(); singleton obj. or neeche wala const dono ka same output hota h
+const tinderUser = {}; // non singleton object
+tinderUser.id = "23fhfj";
+tinderUser.name = "sam";
+tinderUser.isLoggedIn = false;
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "rohit",
+            lastname: "bajpai"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj4 = {5: "a", 6: "b"};
+//const obj3 = {obj1, obj2};
+//const obj3 = Object.assign({}, obj1, obj2, obj4);
+const obj3 = {...obj1, ...obj2} // combining objects using spread operator
+//console.log(obj3);
+
+const user = [
+    {
+        id1: 1,
+        email: "hot@email.com" 
+    },
+    {
+        id1: 1,
+        email: "hot@email.com" 
+    },
+    {
+        id1: 1,
+        email: "hot@email.com" 
+    }
+
+
+]
+
+user[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
 
 
 
